862b34d2129c5211e0fd2c15c66f8c26822b94e5
deniseradstaat <90279019+deniseradstaat@users.noreply.github.com>
2021-09-28 22:31:26 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,27 @@
 // JavaScript Document
 
+var marioThema;
 var luigiThema;
+var peachThema;
 var body;
 
+marioThema = document.querySelector("section:last-of-type article:first-of-type");
 luigiThema = document.querySelector("section:last-of-type article:nth-of-type(2)");
+peachThema = document.querySelector("section:last-of-type article:nth-of-type(3)");
 body = document.querySelector("body");
 
+marioThema.addEventListener("click", doeRodeThema);
 luigiThema.addEventListener("click", doeGroeneThema);
+peachThema.addEventListener("click", doeRozeThema);
+
+function doeRodeThema() {
+    body.classList.toggle("mario");
+}
 
 function doeGroeneThema() {
-    body.classList.add("luigi");
+    body.classList.toggle("luigi");
+}
+
+function doeRozeThema() {
+    body.classList.toggle("peach");
 }
