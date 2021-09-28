3cee1931535af128de54a4d20b7f02fcbfbf03ea
deniseradstaat <90279019+deniseradstaat@users.noreply.github.com>
2021-09-28 22:04:38 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+
+var luigiThema;
+var body;
+
+luigiThema = document.querySelector("section:last-of-type article:nth-of-type(2)");
+body = document.querySelector("body");
+
+luigiThema.addEventListener("click", doeGroeneThema);
+
+function doeGroeneThema() {
+    body.classList.add("luigi");
+}
